5922c429247ba8d0dd8541a8ea6a48a882adda38
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-12-16 19:38:35 -0600

    add a new transaction function and pass index

--- blockchain.py
+++ blockchain.py
@@ -7,8 +7,21 @@ class Blockchain(object):
         # Creates a new Block and adds it to the chain
         pass
 
-    def new_transaction(self):
+    def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list of transactions
+        # Creates a new transaction to go into the next mined Block
+        """
+        :param sender: <str> Address of the sender
+        :param recipient: <str> Address of recipient
+        :param amount: <int> amount
+        :return: <int> The index of the Block will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amunt': amount,
+        })
         pass
 
     @staticmethod
