d0bdd86bc499d8e0d906bcf3ffd57e0677d83bb1
Sarah Colby <sarah@Sarahs-MacBook-Pro.local>
2017-12-16 19:48:20 -0600

    function initializes with first block

--- blockchain.py
+++ blockchain.py
@@ -1,16 +1,39 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transaction = []
 
     def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
+        """
+        Creates a new Block and adds it to the chain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
+        """
         # Adds a new transaction to the list of transactions
         # Creates a new transaction to go into the next mined Block
-        """
         :param sender: <str> Address of the sender
         :param recipient: <str> Address of recipient
         :param amount: <int> amount
@@ -22,7 +45,8 @@ class Blockchain(object):
             'recipient': recipient,
             'amunt': amount,
         })
-        pass
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
